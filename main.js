ddf1f1eb3bdb0c0253f3e49c9122090b61ae8925
Subir Kumaar <subirpratihar2000@gmail.com>
2026-08-03 20:55:27 +0530

    set random color on h1 after 1 sec.

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const body = document.querySelector('body');
+
+setInterval(() => {
+  let fontRed = Math.round(Math.random()*255);
+  let fontGreen = Math.round(Math.random()*255);
+  let fontBlue = Math.round(Math.random()*255);
+  
+
+  let backRed = Math.round(Math.random()*255);
+  let backGreen = Math.round(Math.random()*255);
+  let backBlue = Math.round(Math.random()*255);
+
+  body.style = `color: rgb(${fontRed}, ${fontGreen}, ${fontBlue})`;
+}, 1000);
