f5d8deb0f64e399d086f12370619305c406f4ea1
Subir Kumar <subirpratihar2000@gmail.com>
2026-08-05 06:23:57 +0000

    feat: add a tool

---
+++ code-merge/board.js
@@ -0,0 +1,38 @@
+const htmlCode = document.querySelector('#html-code');
+const cssCode = document.querySelector('#css-code');
+const jsCode = document.querySelector('#js-code');
+const jsProblem = document.querySelector('#js-problem');
+const prompt = document.querySelector('#prompt');
+
+document.querySelectorAll('#field-btn li')[0].classList.add('js-active');
+
+document.querySelector('#generate-btn').addEventListener('click', () => {
+  let newCode;
+  newCode = htmlCode.value.replace('<link rel="stylesheet" href="style.css">', `<style>
+  ${cssCode.value}
+  </style>`);
+  
+  newCode = newCode.replace('<script src="main.js"></script>', `<script>
+  ${jsCode.value}
+  </script>`);
+  
+  prompt.value = `"${newCode}" - is JavaScript solution is prefect for the bellow problem. "${jsProblem.value}"`;
+});
+
+for (let i = 0; i < 4; i++) {
+  document.querySelectorAll('#field-btn li')[i].addEventListener('click',(event) => {
+    
+    let fieldBtnId = event.target.dataset.id;
+    
+    for(let i=0; i<4; i++){
+      document.querySelectorAll('#field-btn li')[i].classList.remove('js-active');
+      document.querySelectorAll('form > div')[i].style = 'display: none;';
+    }
+    document.getElementById(fieldBtnId).style = 'display: flex;';
+    event.target.classList.add('js-active');
+  });
+}
+
+document.querySelector('#copy-pompt').addEventListener('click', () => {
+  navigator.clipboard.writeText(prompt.value);
+});
